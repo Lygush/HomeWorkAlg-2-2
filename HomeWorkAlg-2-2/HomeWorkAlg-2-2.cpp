7b577364bf728dd44388012d7d72a1cfc9257ef6
Ivan <kosarevsky2000@yandex.ru>
2023-07-18 21:23:42 +0300

    initial commit

---
+++ HomeWorkAlg-2-2/HomeWorkAlg-2-2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+unsigned long long int Fibonacci(int n, int* mass) {
+    if (mass[n] != 0) {
+        return mass[n];
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    mass[n] = Fibonacci(n - 1, mass) + Fibonacci(n - 2, mass);
+    return mass[n];
+}
+
+int main()
+{
+    int number{};
+    std::cin >> number;
+    if (number == 0) {
+        std::cout << 0;
+    }
+    else {
+        int* mass = new int [number + 1] {0};
+        std::cout << Fibonacci(number, mass) << std::endl;
+    }
+    return 0;
+}
